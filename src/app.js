2da2dd68e1817fa44acefbef27640db577b1ce1e
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-12-17 01:13:05 -0300

    feat: Crea estructura de carpeta para la practica de Web server
    En src crea:
    - Crea app.js
    - Carpeta config para la configuracion de nuestras variables de entorno
     - Crea env.js dentro
    - Carpeta server toda la configuracion del servidor
     - Crea server.js dentro

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+const main = () => {
+    console.log("Aqui comenzaremos nuestro servidor");
+}
+
+// funcion agnostica autocombocada
+// agnostica por que no tiene nombre
+// autocombocada por que la autoejecutamos con los parenteci
+
+(async() =>{
+    main();
+})
