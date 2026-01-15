41d73e97ae7982ee13c0f5b8a6b5aac8f488a726
Matias <matiasnicolaslazarte@gmail.com>
2026-01-15 07:15:47 -0300

    refactor: add app.js and install dependenci mongoose, express,dotenv

--- src/app.js
+++ src/app.js
@@ -1,31 +1,10 @@
-/*arranque de la aplicacion*/
-const {envs} = require("./config/env") // lee el archivo .env y lo trasforma en JS - Node lee -env y devuelve un objeto JS usable
-const {startServer} = require("./server/server")
+const express = require("express");
+const {config} = require("dotenv");
+config();
 
-const main = () => {
-    console.log("Aqui comenzaremos nuestro servidor");
-    console.log(envs);
-    console.log("aqui llamo a startServer");
+const app = express();
+const port = process.env.PORT || 3000;
 
-    /*
-    App.js arranca
-    importa env y startServer
-    Node.js lee los archivos y los trasforma en objeto js
-    ejecuta la main()
-    main() llama ahora abajo a startServer
-    startServer recibe puerto y path.
-     */
-    startServer({
-        port: envs.PORT,
-        public_path: envs.PUBLIC_PATH
-    })
-}
-
-
-// funcion agnostica autocombocada
-// agnostica por que no tiene nombre
-// autocombocada por que la autoejecutamos con los parenteci
-
-(async() =>{
-    main();
-})();
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
