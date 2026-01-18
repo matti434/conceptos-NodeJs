761e4ef61b567dcfc0e40f3109b7c03a79e011f8
Matias <matiasnicolaslazarte@gmail.com>
2026-01-18 02:08:50 -0300

    refactor: project init with react + vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Exercise1 from './ui/screen/exercise-1'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conceptos NodeJs</h1>
+      <Exercise1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/ui/screen/exercise-1.jsx
@@ -0,0 +1,12 @@
+import '../disign/theme-1.css'
+
+function Exercise1() {
+  return (
+    <div className="exercise-1">
+      <h2>Ejercicio 1</h2>
+      <p>Componente de ejercicio 1</p>
+    </div>
+  )
+}
+
+export default Exercise1
